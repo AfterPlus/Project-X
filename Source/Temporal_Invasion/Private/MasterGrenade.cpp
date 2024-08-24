6b9f0ff39fbc21014b22028d3990e8c870a6a643
Sanjay Raj <103418247+AfterPlus@users.noreply.github.com>
2024-08-24 17:58:13 +0530

    grenade collision updates

--- Source/Temporal_Invasion/Private/MasterGrenade.cpp
+++ Source/Temporal_Invasion/Private/MasterGrenade.cpp
@@ -5,37 +5,44 @@
 #include "Components/SphereComponent.h"
 #include "Core/MyCharacter.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AMasterGrenade::AMasterGrenade()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 	Mesh->SetupAttachment(RootComponent); // Attach the Mesh to the root component
 	
-	// Create the Collision Box and Mesh component
 	CollisionBox = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionBox"));
 	CollisionBox->SetupAttachment(Mesh);  // Set the CollisionBox as the mesh
-
-	//ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	
+	MeshCollision = CreateDefaultSubobject<USphereComponent>(TEXT("MeshCollision"));
+	MeshCollision->SetupAttachment(Mesh);  // Set the MeshCollision as the mesh
 	
 	// Set up the overlap event
 	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AMasterGrenade::OnOverlapBegin);
+	MeshCollision->OnComponentBeginOverlap.AddDynamic(this,&AMasterGrenade::OnOverlapBegin_MeshCollision);
 	
 	// Enable collision
 	// CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 	// CollisionBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
 	// CollisionBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
 	// CollisionBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Mesh->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
+	Mesh->SetCollisionResponseToChannels(ECollisionResponse::ECR_Overlap);
+	Mesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
 }
 
 // Called when the game starts or when spawned
 void AMasterGrenade::BeginPlay()
 {
 	Super::BeginPlay();
-	UpdateGrenadeState(EGrenadeState::EIR_InHand);
+	UpdateGrenadeState(GrenadeState);
 }
 
 // Called every frame
@@ -55,17 +62,39 @@ void AMasterGrenade::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor*
 	}
 }
 
+void AMasterGrenade::OnOverlapBegin_MeshCollision(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+		// If it's not a character or the component doesn't belong to the character, play the bounce sound
+		if (BounceSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, BounceSound, SweepResult.Location);
+		}
+	
+}
+
 void AMasterGrenade::UpdateGrenadeState_Implementation(EGrenadeState WeaponState)
 {
-	switch (GrenadeState)
+	switch (WeaponState)
 	{
 	case EGrenadeState::EIR_InHand:
+		// Collision Box
 		CollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		// Mesh Collision
+		//MeshCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 		break;
 
 	case EGrenadeState::EIR_OnGround:
+		// Collision Box
 		CollisionBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
 		CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		// Mesh Collision
+		//MeshCollision->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+		//MeshCollision->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		break;
+
+	default:
 		break;
 	}
 }
+
--- Source/Temporal_Invasion/Public/MasterGrenade.h
+++ Source/Temporal_Invasion/Public/MasterGrenade.h
@@ -37,11 +37,17 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Component")
 	USphereComponent* CollisionBox;
 
-	//UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Component")
-	//UProjectileMovementComponent* ProjectileMovement;
-
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Component")
+	USphereComponent* MeshCollision;
+	
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Variable")
 	EGrenadeState GrenadeState;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Variable")
+	USoundBase* BounceSound;
+
+	UPROPERTY(VisibleAnywhere)
+	UAudioComponent* AudioComponent;
 	
 
 protected:
@@ -54,6 +60,9 @@ public:
 
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
+	UFUNCTION()
+	void OnOverlapBegin_MeshCollision(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Functions")
 	void UpdateGrenadeState(EGrenadeState WeaponState);
